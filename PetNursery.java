2dfda31ab28498a914c4b70bafbea800ac982dc0
rsalee27 <146591878+rsalee27@users.noreply.github.com>
2023-10-16 20:09:23 -0400

    Add files via upload

---
+++ PetNursery.java
@@ -0,0 +1,37 @@
+public class PetNursery {
+    
+    public static void main(String[]args) {
+        
+
+        interface Animals {
+            // This class will create a pet nursery for puppies and kittens
+            // This program should be able to print the info for the pets currently in the nursery
+         }
+
+         Animal puppies= object;
+         Animal kittens= object;
+
+         System.out.println("Welcome to the pet nursery.");
+
+
+         class puppies extends Animal
+        {
+            public void amount() {
+            int number = 1 + (int)(100
+             * Math.random());
+             System.out.println("There are" + puppies + "currently in the nursery.");
+             public void animalSound(bark); 
+
+        }   
+
+        class kittens extends Animal 
+        {
+
+
+        }
+
+        
+
+    }
+}
+}
