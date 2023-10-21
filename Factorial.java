fc7b9c8933ca057afa2dd0ee38488758cf1c30e4
rsalee27 <146591878+rsalee27@users.noreply.github.com>
2023-10-21 11:14:32 -0400

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,13 +1,15 @@
-public class Factorial {
-    public static void main(String[]args)
-    
-    {
-    int n, i, fact=1;
-    n= 6 // I'm finding the factorial of 5.
-    for(i=n; i>1; --i) {
+import java.math.BigInteger;
 
-        fact=fact*i;
-    }
-    System.out.print(fact);
+public class Factorial {
+ 
+    public static void main(String[]args) {
+
+        for (int i=1; i<101; i++){
+            BigInteger facto = BigInteger.valueOf(1);
+            for(int j = i; j > 1; --j) {
+                facto = BigInteger.valueOf(j).multiply(facto);
+            }
+            System.out.println(facto);
+        }
     }
 }
